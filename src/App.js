695a203aa9c9a7a044fa79b10a92c0892879b248
Anchor <zjm8606@163.com>
2020-11-01 21:41:48 +0800

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>44</code> and save to reload.
+           <code>444</code> and save to reload.
         </p>
         <a
           className="App-link"
